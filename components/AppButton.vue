af87c352a298284666cbf47def169d968d2b7cc7
Chrisu892 <chris.prusakiewicz@gmail.com>
2023-02-14 20:41:59 +0000

    initial config

---
+++ components/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="button">
+    Button!
+  </button>
+</template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    App Header
+  </header>
+</template>
+
+<style lang="scss">
+  .header {
+    background-color: $clr-primary;
+  }
+</style>
